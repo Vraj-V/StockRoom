076c34ac19af9adf9b65e8a702667591030f56f3
Vraj-V <vrajv2108@gmail.com>
2026-09-21 07:49:33 +0530

    connected

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./route/ProductRoute');
 const { signup, login, logout } = require('./controllers/Auth_cntrl');
-
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
